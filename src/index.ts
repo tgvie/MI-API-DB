fb24dc89a01e69fb5b81a7a107acbb2474bf8cce
tgvie <166875380+tgvie@users.noreply.github.com>
2025-04-25 22:34:34 +0200

    🗃️ add global error handler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import productRoutes from './routes/productRoutes';
 import categoryRoutes from './routes/categoryRoutes';
 
@@ -10,6 +10,15 @@ app.use(express.json());
 // API endpoint base
 app.use('/products', productRoutes);
 app.use('/categories', categoryRoutes);
+
+// Global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    console.error('Global error handler:', error);
+
+    res.status(error.status || 500).json({
+        message: error.message || 'Internal server error'
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
